254e799dde6a7459a76ebfbe25f35f0abfcdbed5
Henry Gunnery <gunneryhenry@gmail.com>
2020-09-21 12:31:13 +0100

    Create user-usertype ara and defined user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  belongs_to :user_type
+
+  def is_admin?
+    self.user_type_id == 1
+  end
+
+  def is_supervisor?
+    self.user_type_id == 2
+  end
+
+  def is_supporter?
+    self.user_type_id == 3
+  end
 end
