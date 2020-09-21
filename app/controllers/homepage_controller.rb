fc456ec2f2d10dae018293473605048f066cff24
Henry Gunnery <gunneryhenry@gmail.com>
2020-09-21 12:55:01 +0100

    Added first steps in homepage controller

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,13 @@
+class HomepageController < ApplicationController
+
+  def index
+    #simple check to make sure that there are users otherwise re-direct to initial_setup
+    if User.first.nil? then
+      redirect_to initial_setup_path
+    end
+  end
+
+  def initial_setup
+    ## TODO: move into seperate controller?
+  end
+end
